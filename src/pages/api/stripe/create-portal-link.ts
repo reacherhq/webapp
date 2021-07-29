9cf16579db8b9ccf6c6281580f94464082ecc114
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-07-29 17:53:32 +0200

    Fix build

--- src/pages/api/stripe/create-portal-link.ts
+++ src/pages/api/stripe/create-portal-link.ts
@@ -1,11 +1,11 @@
 import { withSentry } from '@sentry/nextjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { getURL } from '../../util/helpers';
-import { sentryException } from '../../util/sentry';
-import { stripe } from '../../util/stripeServer';
-import { getUser } from '../../util/supabaseServer';
-import { createOrRetrieveCustomer } from '../../util/useDatabase';
+import { getURL } from '../../../util/helpers';
+import { sentryException } from '../../../util/sentry';
+import { stripe } from '../../../util/stripeServer';
+import { getUser } from '../../../util/supabaseServer';
+import { createOrRetrieveCustomer } from '../../../util/useDatabase';
 
 const createPortalLink = async (
 	req: NextApiRequest,
