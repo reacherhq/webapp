a807b8781f19e67d4f2c0ce39419a81782d2afe4
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-07-30 21:49:36 +0200

    Fix sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,8 +4,13 @@
 
 import * as Sentry from '@sentry/nextjs';
 
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
 Sentry.init({
-	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+	dsn: SENTRY_DSN,
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1.0,
+	// ...
 	// Note: if you want to override the automatic release value, do not set a
 	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
 	// that it will also get attached to your source maps
