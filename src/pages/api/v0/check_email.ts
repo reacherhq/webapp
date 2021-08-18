dc18e41186adb14c51dedecf9f16f25dc8e262a9
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-08-18 23:17:57 +0200

    fix: Allow CORS from any origin

--- src/pages/api/v0/check_email.ts
+++ src/pages/api/v0/check_email.ts
@@ -1,6 +1,7 @@
 import { withSentry } from '@sentry/nextjs';
 import { User } from '@supabase/supabase-js';
 import axios, { AxiosError } from 'axios';
+import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { sentryException } from '../../../util/sentry';
@@ -16,10 +17,45 @@ import {
 // Reacher only exposes one endpoint right now, so we're hardcoding it.
 const ENDPOINT = `/v0/check_email`;
 
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+//
+// Copied from https://github.com/vercel/next.js/blob/a342fba00ac0fa57f2685665be52bf42649881b6/examples/api-routes-cors/lib/init-middleware.js
+// MIT License Copyright (c) 2021 Vercel, Inc.
+function initMiddleware<R>(
+	middleware: (
+		req: NextApiRequest,
+		res: NextApiResponse,
+		cb: (r: R) => void
+	) => void
+): (req: NextApiRequest, res: NextApiResponse) => Promise<R> {
+	return (req, res) =>
+		new Promise((resolve, reject) => {
+			middleware(req, res, (result) => {
+				if (result instanceof Error) {
+					return reject(result);
+				}
+				return resolve(result);
+			});
+		});
+}
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+	// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+	Cors({
+		// Only allow requests with GET, POST, OPTIONS and HEAD
+		methods: ['GET', 'POST', 'OPTIONS', 'HEAD'],
+	})
+);
+
 const checkEmail = async (
 	req: NextApiRequest,
 	res: NextApiResponse
 ): Promise<void> => {
+	// Run cors
+	await cors(req, res);
+
 	if (req.method !== 'POST') {
 		res.setHeader('Allow', 'POST');
 		res.status(405).json({ error: 'Method Not Allowed' });
