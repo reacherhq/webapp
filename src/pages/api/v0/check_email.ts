cc43e047a69fcd48bfbd72dc6ff5f47e2c0d9ea8
Amaury <1293565+amaurym@users.noreply.github.com>
2022-02-17 10:15:09 +0100

    fix: Turn off public endpoint for now (#170)
    * fix: Turn off public endpoint for now
    
    * Add return
    
    * More details

--- src/pages/api/v0/check_email.ts
+++ src/pages/api/v0/check_email.ts
@@ -88,6 +88,13 @@ const checkEmail = async (
 
 		// Handle the landing page demo token.
 		if (token === TEST_API_TOKEN) {
+			res.status(401).json({
+				error:
+					'Reacher is turning off the public endpoint to prevent spam abuse. Please create a free Reacher account for now for 50 emails / month, until an anti-spam measure has been deployed.',
+			});
+
+			return;
+
 			try {
 				const rateLimiterRes = await rateLimiter.consume(
 					getClientIp(req) || 'FALLBACK_IP',
