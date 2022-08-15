37b75d0b59666a0615ef6901761d6613a0d714ae
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-08-15 17:08:35 +0200

    fix: Fix SQL migration script

--- supabase/migrations/20220815154100_uuid_per_verif.sql
+++ supabase/migrations/20220815154100_uuid_per_verif.sql
@@ -1,6 +1,6 @@
 -- Add a verification_id UUID column, which is unique per email verification.
 ALTER TABLE IF EXISTS public.calls
-ADD COLUMN verification_id uuid not null unique default uuid_generate_v1();
+ADD COLUMN verification_id uuid not null default uuid_generate_v4();
 
 -- Store the email domain name for debugging and analytics purpose.
 ALTER TABLE IF EXISTS public.calls
