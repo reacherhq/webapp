98fb0b76b5d5e399dadde77cda99f90812ac6836
Amaury <1293565+amaurym@users.noreply.github.com>
2022-11-09 21:20:23 +0100

    feat: Black Friday 2022 (#370)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,6 +8,7 @@ import React from 'react';
 import logo from '../assets/logo/reacher.svg';
 import { sentryException } from '../util/sentry';
 import { useUser } from '../util/useUser';
+import { Bf2002 } from './Bf2022';
 import styles from './Nav.module.css';
 
 export function Nav(): React.ReactElement {
@@ -15,66 +16,69 @@ export function Nav(): React.ReactElement {
 	const router = useRouter();
 
 	return (
-		<header className={styles.container}>
-			<div>
-				<a
-					className="flex"
-					href={user ? '/dashboard' : 'https://reacher.email'}
-				>
-					<Image height={24} src={logo} width={24} />
-					<Text className={styles.reacher} h3>
-						Reacher
-						{user && (
-							<Text
-								className={styles.dashboard}
-								span
-								type="secondary"
-							>
-								Dashboard
-							</Text>
-						)}
-					</Text>
-					<Spacer x={0.5} />
-				</a>
-			</div>
-
-			<div className={styles.filler} />
-
-			<div>
-				<GLink
-					className={styles.link}
-					href="/pricing"
-					data-sa-link-event="nav:pricing:click"
-				>
-					Pricing
-				</GLink>
-
-				<GLink
-					className={styles.link}
-					href="https://help.reacher.email"
-					target="_blank"
-					rel="noopener noreferrer"
-					data-sa-link-event="nav:help:click"
-				>
-					Help Center
-				</GLink>
-			</div>
-			{user && (
-				<Select
-					className={styles.dropdown}
-					placeholder={userDetails?.full_name || user.email}
-				>
-					<Select.Option
-						onClick={() => {
-							signOut()
-								.then(() => router.push('/login'))
-								.catch(sentryException);
-						}}
+		<>
+			<Bf2002 />
+			<header className={styles.container}>
+				<div>
+					<a
+						className="flex"
+						href={user ? '/dashboard' : 'https://reacher.email'}
 					>
-						Log Out{' '}
-					</Select.Option>
-				</Select>
-			)}
-		</header>
+						<Image height={24} src={logo} width={24} />
+						<Text className={styles.reacher} h3>
+							Reacher
+							{user && (
+								<Text
+									className={styles.dashboard}
+									span
+									type="secondary"
+								>
+									Dashboard
+								</Text>
+							)}
+						</Text>
+						<Spacer x={0.5} />
+					</a>
+				</div>
+
+				<div className={styles.filler} />
+
+				<div>
+					<GLink
+						className={styles.link}
+						href="/pricing"
+						data-sa-link-event="nav:pricing:click"
+					>
+						Pricing
+					</GLink>
+
+					<GLink
+						className={styles.link}
+						href="https://help.reacher.email"
+						target="_blank"
+						rel="noopener noreferrer"
+						data-sa-link-event="nav:help:click"
+					>
+						Help Center
+					</GLink>
+				</div>
+				{user && (
+					<Select
+						className={styles.dropdown}
+						placeholder={userDetails?.full_name || user.email}
+					>
+						<Select.Option
+							onClick={() => {
+								signOut()
+									.then(() => router.push('/login'))
+									.catch(sentryException);
+							}}
+						>
+							Log Out{' '}
+						</Select.Option>
+					</Select>
+				)}
+			</header>
+		</>
 	);
 }
