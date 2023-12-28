0aef8a63849aa756610cecbbb71c646bc5a358c1
Amaury <1293565+amaury1729@users.noreply.github.com>
2023-12-28 21:10:02 +0100

    Fix supabase

--- src/supabase/supabaseServer.ts
+++ src/supabase/supabaseServer.ts
@@ -2,52 +2,51 @@
 // https://github.com/vercel/nextjs-subscription-payments/blob/c7867b2d9e08d033056293d12aeb9825b8331806/app/supabase-server.ts
 // License: MIT
 
+import { ProductWithPrice } from "./domain.types";
 import { supabaseAdmin } from "./supabaseAdmin";
 
 export async function getSession() {
 	const supabase = supabaseAdmin;
-	try {
-		const {
-			data: { session },
-		} = await supabase.auth.getSession();
-		return session;
-	} catch (error) {
-		console.error("Error:", error);
-		return null;
+	const {
+		data: { session },
+		error,
+	} = await supabase.auth.getSession();
+
+	if (error) {
+		throw error;
 	}
+	return session;
 }
 
 export async function getUserDetails() {
 	const supabase = supabaseAdmin;
-	try {
-		const { data: userDetails } = await supabase
-			.from("users")
-			.select("*")
-			.single();
-		return userDetails;
-	} catch (error) {
-		console.error("Error:", error);
-		return null;
+	const { data: userDetails, error } = await supabase
+		.from("users")
+		.select("*")
+		.single();
+	return userDetails;
+	if (error) {
+		throw error;
 	}
 }
 
 export async function getSubscription() {
 	const supabase = supabaseAdmin;
-	try {
-		const { data: subscription } = await supabase
-			.from("subscriptions")
-			.select("*, prices(*, products(*))")
-			.in("status", ["trialing", "active"])
-			.maybeSingle()
-			.throwOnError();
-		return subscription;
-	} catch (error) {
-		console.error("Error:", error);
-		return null;
+	const { data: subscription, error } = await supabase
+		.from("subscriptions")
+		.select("*, prices(*, products(*))")
+		.in("status", ["trialing", "active"])
+		.maybeSingle()
+		.throwOnError();
+	if (error) {
+		throw error;
 	}
+	return subscription;
 }
 
-export const getActiveProductsWithPrices = async () => {
+export const getActiveProductsWithPrices = async (): Promise<
+	ProductWithPrice[]
+> => {
 	const supabase = supabaseAdmin;
 	const { data, error } = await supabase
 		.from("products")
@@ -58,7 +57,7 @@ export const getActiveProductsWithPrices = async () => {
 		.order("unit_amount", { foreignTable: "prices" });
 
 	if (error) {
-		console.log(error.message);
+		throw error;
 	}
 	return data ?? [];
 };
