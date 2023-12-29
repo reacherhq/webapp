752c190cb7ed545d99889a9315d66643a94e3c42
Amaury <1293565+amaury1729@users.noreply.github.com>
2023-12-29 18:11:34 +0100

    Finish sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
+import { useEffect } from "react";
+
 export default function GlobalError({
 	error,
 }: {
 	error: Error & { digest?: string };
 }) {
+	useEffect(() => {
+		Sentry.captureException(error);
+	}, [error]);
+
 	return (
 		<html>
 			<body>
-				<h2>Something went wrong!</h2>
-				<p>{error.message}</p>
+				<h2>
+					Something went wrong. Please email a screenshot of this page
+					to amaury@reacher.email, thank you!
+				</h2>
+				<p>Error: {JSON.stringify(error)}</p>
 				<p>{error.digest}</p>
 			</body>
 		</html>
