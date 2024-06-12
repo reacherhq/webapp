11540a9f3ee7a4c378a36d19aba045ea648d55c1
Storm <storm@reacher.email>
2024-06-12 17:14:04 +0200

    fix: Fix sub_and_calls sql

--- supabase/migrations/20240116132530_new_sub_and_calls.sql
+++ supabase/migrations/20240116132530_new_sub_and_calls.sql
@@ -19,13 +19,13 @@ AS
     LEFT JOIN
         prices pri ON pri.id = s.price_id
     LEFT JOIN
-        products pro on pro.id = pri.product_id
+        products pro ON pro.id = pri.product_id
     LEFT JOIN
         calls c ON u.id = c.user_id
         AND (
             (s.current_period_start IS NOT NULL AND s.current_period_end IS NOT NULL AND c.created_at BETWEEN s.current_period_start AND s.current_period_end)
             OR
-            (c.created_at >= NOW() - INTERVAL '1 MONTH')
+            (s.current_period_start IS NULL OR s.current_period_end IS NULL) AND (c.created_at >= NOW() - INTERVAL '1 MONTH')
         )
     GROUP BY
-        u.id, s.id, s.current_period_end, pro.id;
+        u.id, s.id, s.status, s.current_period_start, s.current_period_end, s.cancel_at, pro.id;
