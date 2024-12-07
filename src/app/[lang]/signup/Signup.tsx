69f0dd5c935704c2b47a8f834761f49ee55152b3
Storm <storm@reacher.email>
2024-12-07 13:07:17 +0100

    Signup

--- src/app/[lang]/signup/Signup.tsx
+++ src/app/[lang]/signup/Signup.tsx
@@ -16,9 +16,20 @@ import { createClient } from "@/supabase/client";
 import { DLink } from "@/components/DLink";
 import { getWebappURL } from "@/util/helpers";
 
+type EMAIL_VOLUME =
+	| "SAAS_10K"
+	| "COMMERCIAL_1M"
+	| "COMMERCIAL_5M"
+	| "COMMERCIAL_10M"
+	| "COMMERCIAL_100M"
+	| "OTHER";
+
 export default function SignUp(props: { d: Dictionary }): React.ReactElement {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [companyWebsite, setCompanyWebsite] = useState("");
+	const [b2bB2c, setB2bB2c] = useState<"B2B" | "B2C" | undefined>();
+	const [emailVoume, setEmailVolume] = useState<EMAIL_VOLUME | undefined>();
 	const [loading, setLoading] = useState(false);
 	const [message, setMessage] = useState<SigninMessage | undefined>(
 		undefined
@@ -32,11 +43,16 @@ export default function SignUp(props: { d: Dictionary }): React.ReactElement {
 	const handleSignup = async () => {
 		setLoading(true);
 		setMessage(undefined);
-		if (email.endsWith("@gmail.com")) {
+		if (
+			email.endsWith("@gmail.com") ||
+			email.endsWith("@yahoo.com") ||
+			email.endsWith("@hotmail.com") ||
+			email.endsWith("@outlook.com")
+		) {
 			setMessage({
 				type: "error",
 				content:
-					"Gmail addresses are currently disabled due to spam abuse. Please use a company email.",
+					"Personal addresses are currently disabled due to spam abuse. Please use a company email.",
 			});
 			setLoading(false);
 			return;
@@ -46,7 +62,12 @@ export default function SignUp(props: { d: Dictionary }): React.ReactElement {
 			email,
 			password,
 			options: {
-				data: feedback ? { heardFrom: feedback } : undefined,
+				data: {
+					b2bB2c,
+					companyWebsite,
+					heardFrom: feedback,
+					emailVoume,
+				},
 				emailRedirectTo: `${getWebappURL()}/auth/callback`,
 			},
 		});
@@ -84,6 +105,21 @@ export default function SignUp(props: { d: Dictionary }): React.ReactElement {
 				>
 					{d.password}
 				</Input.Password>
+				<Spacer />
+				<Text i p small>
+					{d.prevent_spam}
+				</Text>
+				<Input
+					placeholder="ex: mycompany.com"
+					onChange={(e) => setCompanyWebsite(e.currentTarget.value)}
+					required
+					type={message?.type}
+					width="100%"
+				>
+					{d.company_website}
+				</Input>
+				<EmailVolume onChange={setEmailVolume} d={d.email_volume} />
+				<B2BB2C onChange={setB2bB2c} d={d.b2b_b2c} />
 				<Feedback onChange={setFeedback} d={d.feedback} />
 				{message && <SigninLayoutMessage message={message} />}
 
@@ -120,6 +156,68 @@ export default function SignUp(props: { d: Dictionary }): React.ReactElement {
 	);
 }
 
+function EmailVolume({
+	onChange,
+	d,
+}: {
+	onChange: (f: EMAIL_VOLUME) => void;
+	d: Dictionary["signup"]["email_volume"];
+}): React.ReactElement {
+	const options: { value: EMAIL_VOLUME; label: string }[] = [
+		{ value: "SAAS_10K", label: `10K ${d.per_month}` },
+		{ value: "COMMERCIAL_1M", label: `1M ${d.per_month}` },
+		{ value: "COMMERCIAL_5M", label: `5M ${d.per_month}` },
+		{ value: "COMMERCIAL_10M", label: `10M ${d.per_month}` },
+		{ value: "COMMERCIAL_100M", label: `100M ${d.per_month}` },
+		{ value: "OTHER", label: d.other },
+	];
+
+	return (
+		<>
+			<Text p small>
+				{d.title}
+			</Text>
+			<Select
+				placeholder="10K, 5M..."
+				onChange={(o) => onChange(o as EMAIL_VOLUME)}
+				width="100%"
+			>
+				{options.map((option) => (
+					<Select.Option key={option.value} value={option.value}>
+						{option.label}
+					</Select.Option>
+				))}
+			</Select>
+		</>
+	);
+}
+
+function B2BB2C({
+	onChange,
+	d,
+}: {
+	onChange: (f: "B2B" | "B2C") => void;
+	d: Dictionary["signup"]["b2b_b2c"];
+}): React.ReactElement {
+	return (
+		<>
+			<Text p small>
+				{d.title}
+			</Text>
+			<Select
+				placeholder="B2B, B2C"
+				onChange={(o) => {
+					onChange(o as "B2B" | "B2C");
+				}}
+				width="100%"
+			>
+				<Select.Option value="B2B">B2B</Select.Option>
+				<Select.Option value="B2C">B2C</Select.Option>
+			</Select>
+		</>
+	);
+}
+
 function Feedback({
 	onChange,
 	d,
@@ -131,7 +229,9 @@ function Feedback({
 
 	return (
 		<>
-			<Text>{d.title}</Text>
+			<Text p small>
+				{d.title}
+			</Text>
 			<Select
 				placeholder={d.placeholder}
 				onChange={(o) => {
